24304d63a7c8428e10926c811b06000642caa12d
Pawel-Sulzycki_lyb <pawel.sulzycki@lyondellbasell.com>
2025-03-14 13:15:38 +0100

    Use reduce instead map, add axis

--- src/features/TransactionChart/hooks/usePrepareEChartOptions.ts
+++ src/features/TransactionChart/hooks/usePrepareEChartOptions.ts
@@ -2,11 +2,27 @@ import { EChartsOption } from "echarts-for-react";
 import { BinanceApi } from "../../types";
 
 export function usePrepareEChartOptions(binanceData: BinanceApi[]): EChartsOption {
-  const priceData = binanceData.map((item) => item.price);
+  const {axis, price} = binanceData.reduce<{price: string[],
+    axis: string[]}>((prev, curr) => {
+    prev.axis.push(new Date(curr.closeTime).toISOString());
+    prev.price.push(curr.price);
+    
+    return prev
+  }, {
+    price: [],
+    axis: []
+  })
   return {
     series: [{
       type: 'line',
-      data: priceData
-  }]
+      data: price,
+  }],
+    xAxis: {
+      type: "category",
+      data: axis
+    },
+    yAxis: {
+      type: "value"
+    },
   }
 }
